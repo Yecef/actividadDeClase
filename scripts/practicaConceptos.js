6d29386973d886f7a09435ba69ec4d8017340104
yecef <yflorez278@gmail.com>
2024-04-16 08:40:54 -0500

    actualizacion

--- scripts/practicaConceptos.js
+++ scripts/practicaConceptos.js
@@ -3,6 +3,26 @@ console.log("Hola, soy la actividad de hoy")
 const edadInput = document.getElementById("edadInput"); 
 console.log(edadInput);
 
+const mensaje = document.getElementById("mensajeEdad");
+console.log(mensaje);
+
+
+const btn = document.getElementById("boton");
+
+console.log(btn);
+
+function verificarEdad(){
+
+//yo quiero que cada vez que le de click al boton, me salga un "hola"
+
+
+console.log("entro a mi funcion verificar edad");
+
+
+
+}
+
+
  
 
 
