5d39f02266b216e2fe4e31da67dc257a2d39367b
yecef <yflorez278@gmail.com>
2024-04-16 10:54:53 -0500

    actualizacion

--- scripts/practicaConceptos.js
+++ scripts/practicaConceptos.js
@@ -19,7 +19,24 @@ function verificarEdad(){
 console.log("entro a mi funcion verificar edad");
 
 
+  //para acceder no al elemento del html sino a su malor, se debe escribir el .value
+  let edad = edadInput.value;
+  console.log(edad);
+//UTILIZAR CONDICIONALES
 
+  if(edad>= 18){
+
+      //acá esto es lo que sucede si la condición es verdadera
+      console.log("Eres mayor de edad");
+
+      mensaje.textContent = "Eres mayor de edad";
+
+  } else{
+      //acá se define lo que sucede cuando la condición es falsa
+      console.log("Eres menor de edad");
+
+      mensaje.textContent = "Eres menor de edad";
+  }
 }
 
 
